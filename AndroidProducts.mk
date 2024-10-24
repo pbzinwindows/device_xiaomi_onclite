2ecbe99ba6c39ff486266eae97992ea689b75e2f
pb <pbzinwindows@gmail.com>
2024-10-24 13:11:52 -0300

    teste

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,6 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_onclite.mk
+    $(LOCAL_DIR)/lineage_onclite.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_onclite-user \
-    nad_onclite-userdebug
+    lineage_onclite-user \
+    lineage_onclite-userdebug
